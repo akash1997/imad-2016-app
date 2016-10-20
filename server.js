d22e7e1495a16bb9f6cd45b187b8f9bb3397eaf0
Akash Agrawal <akashrocksha@gmail.com>
2016-10-21 01:24:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,20 +5,24 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/article-one', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-two', function (req, res) {
+app.get('/article-one', function (req, res) {
   res.send("Article one will be displayed shortly.");
 });
 
-app.get('/article-three', function (req, res) {
+app.get('/article-two', function (req, res) {
   res.send("Article two will be displayed shortly.");
 });
 
+app.get('/article-three', function (req, res) {
+  res.sendFile("Article three will be displayed shortly.");
+});
+
 app.get('/ui/style.css', function (req, res) {
-  res.send("Article three will be displayed shortly.");
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
