2d6275f2cb87aa08521c0872a9813dfb092be2e9
Akash Agrawal <akashrocksha@gmail.com>
2016-10-23 02:26:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,10 +92,10 @@ app.get('/article-three', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });*/
 
-/*app.get('/:articleName', function (req, res) {
+app.get('/:articleName', function (req, res) {
   var articleName=req.params.articleName;
   res.send(createTemplate(articles[articleName]));
-});*/
+});
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
