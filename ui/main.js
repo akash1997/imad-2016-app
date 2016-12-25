982389e558c0648da5f21e772efc9c81d33aee7a
Akash Agrawal <akashrocksha@gmail.com>
2016-12-25 12:40:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ element.innerHTML = 'New Value';
 var img = document.getElementById('madi');
 function moveRight () {
     marginLeft = marginLeft + 10;
-    img.style.marginLeft = marginLeft + "px";
+    img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function () {
     var interval = setInterval(moveRight,100);
