6f7f227748c6d2a9052fda93807c008900dc1b0f
Akash Agrawal <akashrocksha@gmail.com>
2016-12-27 11:54:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 //var element = document.getElementById('main-text');
 //element.innerHTML = 'New Value';
 
-var img = document.getElementById('madi');
+/*var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight () {
     marginLeft = marginLeft + 1;
@@ -11,4 +11,15 @@ function moveRight () {
 }
 img.onclick = function () {
     var interval = setInterval(moveRight,50);
+};*/
+
+var button = document.getElementById('counter');
+button.onclick = function() {
+    //Make a request to the counter endpoint
+    
+    //Capture the response and store it in variable
+    
+    //render the variable in the correct span
+    counter = counter + 1;
+    span.innerHTML = counter.toString();
 };
